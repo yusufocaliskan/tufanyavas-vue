ec6821c2e1a00bf4b248cdb14c539f454cb77ad9
Yusuf Çalışkan <yusufocaliskan@gmail.com>
2023-03-31 13:32:53 +0300

    Switcher ve sub-cagories eklendi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,6 +51,44 @@
           <!-- Profile dropdown -->
           <Menu as="div" class="relative ml-3">
             <div class="flex items-center space-x-5">
+              <div>
+                <button
+                  id="theme-toggle"
+                  data-tooltip-target="tooltip-toggle"
+                  type="button"
+                  class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+                >
+                  <svg
+                    aria-hidden="true"
+                    id="theme-toggle-dark-icon"
+                    class="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+                    ></path>
+                  </svg>
+                  <svg
+                    aria-hidden="true"
+                    id="theme-toggle-light-icon"
+                    class="hidden w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only">Toggle dark mode</span>
+                </button>
+              </div>
+              <!-- Dark-Light Mode switcher -->
+
               <MenuButton
                 class="flex items-center space-x-3 rounded-full text-sm focus:outline-none"
               >
@@ -145,7 +183,24 @@
       <div
         class="main-content flex w-full sm:w-5/5 lg:w-4/5 md:w-5/5 xl:w-4/5 overflow-y-auto bg-yellow-400"
       >
-        CONTENT
+        <!-- Sub Categories -->
+        <div class="sub-categories flex w-full h-16 pt-2 sm:block bg-white">
+          <div class="flex justify-center space-x-4">
+            <a
+              v-for="item in sub_categories"
+              :key="item.name"
+              :href="item.href"
+              :class="[
+                item.current
+                  ? ' pb-3 border-b-4 border-blue-500'
+                  : 'text-gray-300 hover:text-black hover:border-b-4 hover:border-blue-500 ',
+                'px-3 py-2 text-sm font-medium',
+              ]"
+              :aria-current="item.current ? 'page' : undefined"
+              >{{ item.name }}</a
+            >
+          </div>
+        </div>
       </div>
       <!-- main:content -->
     </div>
@@ -173,5 +228,10 @@ const navigation = [
   { name: "Forex", href: "#", current: true },
   { name: "Crypto", href: "#", current: false },
   { name: "Elliot Wave School", href: "#", current: false },
+];
+
+const sub_categories = [
+  { name: "Charts", href: "#", current: true },
+  { name: "Video Analysis", href: "#", current: false },
 ];
 </script>

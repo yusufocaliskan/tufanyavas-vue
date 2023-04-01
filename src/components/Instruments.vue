b2d6fb88c7b6cbcdc4df85a1b59bb4b993022485
Yusuf Çalışkan <yusufocaliskan@gmail.com>
2023-04-01 21:54:46 +0300

    Aramalar için go_back methodu yazıldı

--- src/components/Instruments.vue
+++ src/components/Instruments.vue
@@ -3,7 +3,7 @@
   <SearchBox
     @search_for="search_for"
     :data="input_data"
-    inputClass="p-4 outline-none border-b-[1px]"
+    class="p-4 outline-none border-b-[1px]"
   />
   <!--end: search-box -->
   <div class="instruments left-content-menu flex overflow-y-auto">
@@ -20,6 +20,21 @@
           {{ item.name }}</a
         >
       </li>
+      <li @click.prevent="($event) => go_back">
+        <a
+          class="text-sm flex w-60 hover:bg-gray-500 hover:text-white font-bold border-b-[1px] text-neutral-900 p-4"
+          href="#"
+        >
+          <BackArrow class="mr-5" /> GO BACK</a
+        >
+      </li>
+      <li class="flex" v-if="list_data.length == 0">
+        <span
+          class="items-center font-regular flex justify-center p-3 text-gray-400"
+        >
+          <NoResult class="mr-2" /> No result</span
+        >
+      </li>
     </ul>
   </div>
 </template>
@@ -31,6 +46,8 @@ import Fobbex from "../store/fobbex.json";
 import SearchBox from "./SearchBox.vue";
 import Star from "./icons/Star.vue";
 import StarFilled from "./icons/StarFilled.vue";
+import NoResult from "./icons/NoResult.vue";
+import BackArrow from "./icons/BackArrow.vue";
 
 const input_data = ref({
   value: "",
@@ -38,7 +55,16 @@ const input_data = ref({
 let list_data = ref(Fobbex);
 
 const search_for = computed(() => {
-  list_data = ref(filteredItems(Fobbex, input_data.value.value));
+  list_data = filteredItems(Fobbex, input_data.value.value);
+});
+
+/**
+ * Arama yapıldıktan sonra gider
+ */
+const go_back = computed(() => {
+  console.log("fdsfs");
+  input_data.value.value = "";
+  list_data = Fobbex;
 });
 
 /**
--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -3,7 +3,6 @@
   <input
     v-model="data.value"
     name="search-box"
-    :class="inputClass"
     :placeholder="placeholder"
     @change="($event) => search_for"
   />
@@ -12,7 +11,6 @@
 
 <script setup>
 const props = defineProps({
-  inputClass: String,
   placeholder: { type: String, default: "Search..." },
   data: Object,
 });
